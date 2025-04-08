a880c25088e9438d7873201f7fdea70a3d8dc4e2
Jafz2001 <120731233+Jafz2001@users.noreply.github.com>
2025-04-08 14:19:06 -0400

    Update team_code.py

--- team_code.py
+++ team_code.py
@@ -203,9 +203,9 @@ def extract_features(record):
 
         # Agregar características adicionales
         base_features['frequency_domain'] = frequency_domain_analysis(normalized_data[:, 1], sfreq)
-        #base_features['nonlinear'] = nonlinear_features(normalized_data[:, 1])
-        #base_features['signal_characteristics'] = additional_signal_characteristics(normalized_data[:, 1])
-        #base_features['wavelet_features'] = complex_wavelet_transform(normalized_data[:, 1])
+        base_features['nonlinear'] = nonlinear_features(normalized_data[:, 1])
+        base_features['signal_characteristics'] = additional_signal_characteristics(normalized_data[:, 1])
+        base_features['wavelet_features'] = complex_wavelet_transform(normalized_data[:, 1])
 
         # Agregar información demográfica
         base_features['demographic'] = {
